462a9445c9adac25ec7575d318ea42079ceeca8f
MuzaffarAbduxamidov <muzaffarismatov2002@gmail.com>
2023-06-21 21:17:49 +0500

    sinfishi20

---
+++ masala2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   int son1=1;
+   int yosh;
+     printf("Yoshingizni kiriting:");
+        scanf("%d", &yosh);
+
+   metka:
+    printf("%d\n", son1);
+     if(son1==yosh){
+     printf("Sizning yoshingiz:%d\n",son1);
+    son1=son1+1
+    } if(son1<=100)
+   goto metka;
+      }
+
+
